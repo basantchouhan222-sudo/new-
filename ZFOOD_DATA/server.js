698b369f6593df952b94316b4ae68ecae9d0bb6f
AN SOLUTIONS <ansolutions@ANs-MacBook-Pro.local>
2026-05-05 21:17:28 +0530

    server Upadte

--- ZFOOD_DATA/server.js
+++ ZFOOD_DATA/server.js
@@ -1,3 +1,29 @@
+// require("dotenv").config();
+
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+
+// app.use(cors());
+// app.use(express.json());
+
+// require("./mongoo");
+
+// const Router = require("./Router");
+// app.use("/api", Router);
+// app.get("/",(req,res)=>{
+//   res.send({
+//     activestatus: true,
+//     error : false,
+//   })
+// })
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+// module.exports = app;
 require("dotenv").config();
 
 const express = require("express");
@@ -11,16 +37,9 @@ require("./mongoo");
 
 const Router = require("./Router");
 app.use("/api", Router);
-app.get("/",(req,res)=>{
-  res.send({
-    activestatus: true,
-    error : false,
-  })
-})
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send({ activestatus: true, error: false });
 });
+
 module.exports = app;
